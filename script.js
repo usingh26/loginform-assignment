a2086eea28c138a2a7f4e36cd8cd45d44ad145ea
Upendra Singh <usingh@deqode.com>
2026-09-16 15:56:32 +0530

    added js for registration

--- script.js
+++ script.js
@@ -29,3 +29,96 @@ usersNavBtn.addEventListener("click", function () {
   loginSection.style.display = "none";
   usersSection.style.display = "block";
 });
+
+//1. handling the register form submission
+
+const registerForm = document.getElementById("register-form");
+const registerMsg = document.getElementById("register-message");
+let users = [];
+
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+
+  const role = document.getElementById("role").value;
+
+  //   validating if gender selected or not
+  const selectedGender = document.querySelector(
+    'input[name="gender"]:checked'
+);
+
+if (!selectedGender) {
+    showRegisterMessage(
+        "Please select your gender!",
+        "error-message"
+    );
+    return;
+}
+
+const gender = selectedGender.value;
+
+const user = {
+    email: email,
+    username: username,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+    gender: gender,
+    role: role,
+};
+
+
+// validating if user already exists
+
+const usernameExists = users.some(function (existingUser) {
+    return existingUser.username === username;
+});
+
+if (usernameExists) {
+    showRegisterMessage(
+        "Username already exists!",
+        "error-message"
+    );
+    return;
+}
+
+
+const emailExists = users.some(function (existingUser) {
+    return existingUser.email === email;
+});
+
+if (emailExists) {
+    showRegisterMessage(
+        "Email already exists!",
+        "error-message"
+    );
+    return;
+}
+
+
+// registration successful
+
+users.push(user);
+
+showRegisterMessage(
+    "Registration successful!",
+    "success-message"
+);
+function showRegisterMessage(message, type) {
+    registerMsg.textContent = message;
+    registerMsg.className = type;
+}
+
+
+// clearing form after successful registration
+
+registerForm.reset();
+
+  // console.log(user);
+});
